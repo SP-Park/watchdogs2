37f47807fcf928b63bafcfeee2995280413bb886
SP-Park <75590347+SP-Park@users.noreply.github.com>
2023-04-11 21:45:33 +0900

    threatX

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.080b76f7c6af3ef00d59faea37c15481.js"
+  "/precache-manifest.c5bfd65da653dbb8a7ee76783b47c5dd.js"
 );
 
 self.addEventListener('message', (event) => {
