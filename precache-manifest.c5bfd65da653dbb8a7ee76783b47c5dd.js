8f7f95635a8e7e7ef3db97981b8b5c70696a8ea4
SP-Park <75590347+SP-Park@users.noreply.github.com>
2023-04-11 21:44:58 +0900

    Updates

---
+++ precache-manifest.c5bfd65da653dbb8a7ee76783b47c5dd.js
@@ -0,0 +1,314 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f3ab698a384022aaeb09422524bd04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f45af8e9d4f5df67ffe",
+    "url": "/static/css/main.c15b21ef.chunk.css"
+  },
+  {
+    "revision": "0a87cf31a51ad16bebca",
+    "url": "/static/js/2.4126e8d0.chunk.js"
+  },
+  {
+    "revision": "595b7e410bc6e766ddc88870bc678fa9",
+    "url": "/static/js/2.4126e8d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f45af8e9d4f5df67ffe",
+    "url": "/static/js/main.44051348.chunk.js"
+  },
+  {
+    "revision": "97d91114e7eaba3350c4",
+    "url": "/static/js/runtime-main.8f950abd.js"
+  },
+  {
+    "revision": "68d8c8231decf756dbe292455c737bdf",
+    "url": "/static/media/2020_wallpaper.68d8c823.jpg"
+  },
+  {
+    "revision": "cb31be8bef0cfb0e2e766129799c292a",
+    "url": "/static/media/2023_Gold.cb31be8b.png"
+  },
+  {
+    "revision": "54a1bd937b077417f7037df1be35de9c",
+    "url": "/static/media/Anti_Cheat.54a1bd93.png"
+  },
+  {
+    "revision": "cea998e0705c1d57b98779ae109f9b6d",
+    "url": "/static/media/CSGEA-2022-Gold-PNG-1.cea998e0.png"
+  },
+  {
+    "revision": "3eda6fda285792b05b1d957bb23ccbec",
+    "url": "/static/media/Cyrex_Pentest.3eda6fda.jpg"
+  },
+  {
+    "revision": "5d7ba0758593f2103807495d6dcdd268",
+    "url": "/static/media/Global Gaming Survey Report -2.5d7ba075.PNG"
+  },
+  {
+    "revision": "b4e227ebf11386e577991cc83056a1ec",
+    "url": "/static/media/Global Gaming Survey Report -3.b4e227eb.PNG"
+  },
+  {
+    "revision": "f08fd1f9836a2b082e34e0f19a1b9760",
+    "url": "/static/media/Global Gaming Survey Report -5.f08fd1f9.PNG"
+  },
+  {
+    "revision": "1efdc4a5f59c14a8f2a9aa33a219fd3f",
+    "url": "/static/media/Global Gaming Survey Report -6.1efdc4a5.PNG"
+  },
+  {
+    "revision": "b33344de8efe6a85b99f5f564dd00e2c",
+    "url": "/static/media/Global Gaming Survey Report.b33344de.PNG"
+  },
+  {
+    "revision": "2014b9ec62522adc2cc914b927820adc",
+    "url": "/static/media/LOOK LIKE.2014b9ec.jpg"
+  },
+  {
+    "revision": "8785856a023eecfa1dbca18714307b2d",
+    "url": "/static/media/STOP.8785856a.jpg"
+  },
+  {
+    "revision": "68f67886cff85735dae6baf83364025e",
+    "url": "/static/media/ThreatX-logo-full-reversed.68f67886.svg"
+  },
+  {
+    "revision": "3c934b3535bcaf01aab34d0cdef91ea6",
+    "url": "/static/media/anti_piracy.3c934b35.png"
+  },
+  {
+    "revision": "458c4096eadd74e7c7b4db01fa837750",
+    "url": "/static/media/anticheat.458c4096.PNG"
+  },
+  {
+    "revision": "281796a3f130a4841cd630e1527c54ab",
+    "url": "/static/media/arrow.281796a3.gif"
+  },
+  {
+    "revision": "f8f0966bb7cd8f16d28f0e7c46b2829b",
+    "url": "/static/media/back-1.f8f0966b.jpg"
+  },
+  {
+    "revision": "8ef41c7c083711b1e58107d69e9c93db",
+    "url": "/static/media/bg-features-1.8ef41c7c.png"
+  },
+  {
+    "revision": "1dcad1402fa335094600993cf4dff4b9",
+    "url": "/static/media/book2.1dcad140.jpg"
+  },
+  {
+    "revision": "9ced74802f6a7c6692f4573657c7a375",
+    "url": "/static/media/cheat.9ced7480.jpg"
+  },
+  {
+    "revision": "fc30a7df8c56e37501f496938a8d9c20",
+    "url": "/static/media/checkmark.fc30a7df.png"
+  },
+  {
+    "revision": "16cdfb6bb369d86b91a073db0dc6d296",
+    "url": "/static/media/child-pc-header.16cdfb6b.jpg"
+  },
+  {
+    "revision": "a061c0cf5d7b2a4217a277149d5fad4f",
+    "url": "/static/media/cinema.a061c0cf.gif"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "aeb75ec4a569ff74e6bfd478709e6a65",
+    "url": "/static/media/cyber-services.aeb75ec4.png"
+  },
+  {
+    "revision": "4639fea254ddf62479add9548ebc77f0",
+    "url": "/static/media/cyberess.4639fea2.jpg"
+  },
+  {
+    "revision": "8dc116acf0ed8e7aae883cedda1be783",
+    "url": "/static/media/cyrex-GDC-20220327.8dc116ac.jpg"
+  },
+  {
+    "revision": "32b2091b8fdd2ea3b9fe3079c0ac1337",
+    "url": "/static/media/cyrex-video-placeholder-1.32b2091b.png"
+  },
+  {
+    "revision": "2fb08f221a866f9f18d36869aa3b3ee1",
+    "url": "/static/media/cyrex1.2fb08f22.PNG"
+  },
+  {
+    "revision": "f53fdc3675ee1287e3276873978f99b8",
+    "url": "/static/media/cyrex_many.f53fdc36.jpg"
+  },
+  {
+    "revision": "3d22eaaebd3757ed239b9b1fab8a6646",
+    "url": "/static/media/dashbord.3d22eaae.png"
+  },
+  {
+    "revision": "c834c0977f1e93f6de549d9b2d5cbcd8",
+    "url": "/static/media/director.c834c097.png"
+  },
+  {
+    "revision": "8f763caf8bda75a6e3f70d2585642954",
+    "url": "/static/media/doom.8f763caf.jpeg"
+  },
+  {
+    "revision": "2d22760923037b776b0b78df2cd0b868",
+    "url": "/static/media/engineering-skills.2d227609.png"
+  },
+  {
+    "revision": "4fb521c5348a243baa88cf737115c158",
+    "url": "/static/media/expatriate_content_Icons_new.4fb521c5.png"
+  },
+  {
+    "revision": "9655e646acdd9a4a375a900b192f8987",
+    "url": "/static/media/expert.9655e646.jpg"
+  },
+  {
+    "revision": "68f5f5ea37e13e94764d67b8277d53c6",
+    "url": "/static/media/gdc-2022.68f5f5ea.png"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/static/media/getFetch.51c934af.cjs"
+  },
+  {
+    "revision": "54e0bac09a495cc924511478e8a1d62b",
+    "url": "/static/media/hack-1.54e0bac0.jpg"
+  },
+  {
+    "revision": "356c6fbad598026b3d22e533e958a065",
+    "url": "/static/media/identity.356c6fba.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "bfad046ae3c2a72f712601c2df495206",
+    "url": "/static/media/intel.bfad046a.jpg"
+  },
+  {
+    "revision": "01ac1f9a9154946101007d54a0f31afc",
+    "url": "/static/media/japan.01ac1f9a.png"
+  },
+  {
+    "revision": "f09f4465b84ce929e1cd07162862566e",
+    "url": "/static/media/korea.f09f4465.png"
+  },
+  {
+    "revision": "191e1d087341184aacbafc7a747e85d3",
+    "url": "/static/media/law.191e1d08.png"
+  },
+  {
+    "revision": "f028df5ef54dcc1ae15242f025d4d948",
+    "url": "/static/media/lion-1.f028df5e.gif"
+  },
+  {
+    "revision": "3d6887068bdce28defe67d2a985abaa1",
+    "url": "/static/media/lion_black.3d688706.jpg"
+  },
+  {
+    "revision": "affc6a22ca3f4463bae1cfb9d81892e9",
+    "url": "/static/media/load2.affc6a22.PNG"
+  },
+  {
+    "revision": "a04f12a02a7c26469ace862e8ef9030c",
+    "url": "/static/media/load3.a04f12a0.PNG"
+  },
+  {
+    "revision": "c9f74f7f1102f9624692e2c32f982245",
+    "url": "/static/media/loadtest.c9f74f7f.PNG"
+  },
+  {
+    "revision": "a12d54a6d473f1c46be4ab2654c17d57",
+    "url": "/static/media/logo1.a12d54a6.png"
+  },
+  {
+    "revision": "3bc1ca4849c354b7d1176368bdd6890b",
+    "url": "/static/media/logo_mini_white.3bc1ca48.png"
+  },
+  {
+    "revision": "f2ef0405ae1cc091f3cb235f280c6317",
+    "url": "/static/media/metin2.f2ef0405.jpg"
+  },
+  {
+    "revision": "94e4d9291528a53bb79778864a1aa282",
+    "url": "/static/media/mobile-game-protection-3.94e4d929.jpg"
+  },
+  {
+    "revision": "c484413353ad5887b20027abbc56f76b",
+    "url": "/static/media/models.c4844133.png"
+  },
+  {
+    "revision": "aadc1701df8ff48f33361210d6fa00eb",
+    "url": "/static/media/mountain.aadc1701.png"
+  },
+  {
+    "revision": "0e901b804dfc256fa26b87b1cbb1d609",
+    "url": "/static/media/pentest.0e901b80.PNG"
+  },
+  {
+    "revision": "f7af8f3d4e0aea07a21657d0a55d8a6c",
+    "url": "/static/media/press.f7af8f3d.gif"
+  },
+  {
+    "revision": "60f7399736b79d9ade4883137502b887",
+    "url": "/static/media/process.60f73997.PNG"
+  },
+  {
+    "revision": "67e798598a1758bddf4cadb5756609bf",
+    "url": "/static/media/purple_bg.67e79859.jpg"
+  },
+  {
+    "revision": "fae75d7d5ffcccd835e8d4f61b9880bb",
+    "url": "/static/media/shutterstock_1005938026-scaled.fae75d7d.jpg"
+  },
+  {
+    "revision": "404a29828d290ddf33f45f2f0f1a09b6",
+    "url": "/static/media/software_protection.404a2982.png"
+  },
+  {
+    "revision": "5f1fa24d133c458ed866443604edcb8f",
+    "url": "/static/media/space-2.5f1fa24d.jpg"
+  },
+  {
+    "revision": "a45e251ad3def173ab781fdf91ea9c01",
+    "url": "/static/media/stars.a45e251a.jpg"
+  },
+  {
+    "revision": "c5025ca8a5dcebded7a13fdde5535978",
+    "url": "/static/media/tamper.c5025ca8.jpg"
+  },
+  {
+    "revision": "bca9179cf2a3df8dddc3b7f8c9624ded",
+    "url": "/static/media/tera.bca9179c.jpeg"
+  },
+  {
+    "revision": "66e145c331f3941f89195faf4fea2fdb",
+    "url": "/static/media/tile.66e145c3.gif"
+  },
+  {
+    "revision": "b8cfeaa7506c19b3114fce5bf9ab2e4b",
+    "url": "/static/media/tracking.b8cfeaa7.gif"
+  },
+  {
+    "revision": "4cf20795366458fcff7f5e767acd17e4",
+    "url": "/static/media/unbotify.4cf20795.jpg"
+  },
+  {
+    "revision": "93cf9ebcbcb9d7ecefeedaf641d62aaa",
+    "url": "/static/media/wave.93cf9ebc.gif"
+  },
+  {
+    "revision": "3e161bcdf68fe3f4aab431c52bda079d",
+    "url": "/static/media/wellbeing_Icons_new.3e161bcd.png"
+  }
+]);
